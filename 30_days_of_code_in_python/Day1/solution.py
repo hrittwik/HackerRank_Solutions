c3fb77fa7203340a725ca697a86beed71d72f8ac
Hrittwik <hrittwik@gmail.com>
2016-12-15 00:13:58 +0600

    Day1: Data Types solved with python

--- 30_days_of_code_in_python/Day1/solution.py
+++ 30_days_of_code_in_python/Day1/solution.py
@@ -3,13 +3,10 @@ d = 4.0
 s = 'HackerRank '
 
 # Declare second integer, double, and String variables.
-
 # Read and save an integer, double, and String to your variables.
 second_integer = int(input())
 second_double = float(input())
 second_string = input()
-#second_integer += i
-#second_double += d
 # Print the sum of both integer variables on a new line.
 print(i + second_integer)
 # Print the sum of the double variables on a new line.
