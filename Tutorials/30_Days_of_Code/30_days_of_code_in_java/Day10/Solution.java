72761450f8594c696a810fb194f02a793e777c4c
hrittwik <hrittwik@gmail.com>
2017-05-09 00:16:35 +0600

    Day 10

---
+++ Tutorials/30_Days_of_Code/30_days_of_code_in_java/Day10/Solution.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be    named Solution. */
+    	Scanner in = new Scanner(System.in);
+    	int num = in.nextInt();
+    	
+    	String decimalToBinary = decimalToBinary(num);
+    	System.out.println(numberOfOne(decimalToBinary));
+    	in.close();
+    }
+    
+    public static String decimalToBinary(int n){
+    	if(n == 0){
+    		return "0";
+    	}
+    	String binary = "";
+    	
+    	while(n > 0){
+    		int remainder = n % 2;
+    		n = n/2;
+    		binary = remainder + binary;
+    	}
+    	return binary;
+    	
+    }
+    
+    public static int numberOfOne(String s){
+    	char one = '1', zero = '0';
+    	char[] binary = s.toCharArray();
+    	int count = 0, consecutive = 0;
+    	
+    	for(int i = 0; i < binary.length; i++){
+    		if(binary[i] == one){
+    			count ++;
+    			if(count > consecutive){
+    				consecutive = count;
+    			}
+    		} else {
+    			count = 0;
+    		}
+    	}
+    	return consecutive;
+    }
+}
